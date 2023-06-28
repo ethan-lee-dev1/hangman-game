d57cf3bef9cbd76950a3465ab74dc022f03b1629
Ethan Lee <elee@xpanxion.com>
2023-06-28 11:14:57 -0400

    finish hangman

--- hangman.py
+++ hangman.py
@@ -1,8 +1,10 @@
 # generate a random word
 from random_word import RandomWords
+from hook import hooks
 r = RandomWords()
 random_word = r.get_random_word()
 current_guess = "_" * len(random_word)
+print(random_word)
 
 # number of tries is 8
 num_of_tries = 8
@@ -22,6 +24,7 @@ def replace(str, guess, list):
     return result
 
 list_of_correct_guess = []
+counter = 0
 # while number of tries > 0 and current guess doesn't have "_"
 while num_of_tries > 0 and "_" in current_guess:
     # current guess
@@ -39,10 +42,14 @@ while num_of_tries > 0 and "_" in current_guess:
         list_of_correct_guess.append(guess)
         current_guess = replace(random_word, guess, list_of_correct_guess)
         print("Good guess!")
+        print("--------------------------------------")
     else:
+        print(hooks[counter])
         print("Oops! That letter is not in the word.")
+        print("--------------------------------------")
         # subtract 1 from number of tries
         num_of_tries -= 1
+        counter += 1
 
     if num_of_tries > 0 and "_" not in current_guess:
         print("You Won!")
@@ -50,3 +57,16 @@ while num_of_tries > 0 and "_" in current_guess:
 
 if num_of_tries == 0:
     print("Game Over!")
+    
+    
+    hangman_pics1 = (
+        '---------\n'
+        '|'
+        '|\n'
+        '|\n'
+        '|\n'
+        '|\n'
+        '-'
+    )
+
+    
